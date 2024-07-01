e5ff17dd1daf0473380cdbe8af049a61a0422302
hayato94087 <hayato94087@gmail.com>
2024-07-02 03:00:04 +0900

    1.4 Prettier の導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { type FC } from "react";
+
 import {
   AlertDialog,
   AlertDialogCancel,
@@ -9,13 +10,12 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-
 import { Button } from "@/components/ui/button";
 
 const Home: FC = () => {
   return (
     <main>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello World</h1>
           <div className="h-4" />
